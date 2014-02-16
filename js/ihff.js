f475b509aa2a3361dc262e7d77609fac89e2e718
Ben Jackson <benjackson84@gmail.com>
2014-02-16 21:52:39 +0000

    new content

--- js/ihff.js
+++ js/ihff.js
@@ -1,6 +1,23 @@
-$.localScroll({
-	target: '#content', // could be a selector or a jQuery object too.
+$.localScroll.hash({
+	target: $('#content'), // could be a selector or a jQuery object too.
 	queue:true,
 	duration:1000,
 	hash:true
+});
+
+$(document).ready(function(){
+	$('.about-nav').on('click',function(event){
+		$('.about-menu > li').removeClass('pure-menu-selected');
+		$(".about-content").fadeOut("fast");
+		$(this).parent('li').addClass('pure-menu-selected');
+		$("#"+$(this).data('tab')+"-content").fadeIn();
+		event.preventDefault();
+	});
+	$('.get-involved-nav').on('click',function(event){
+		$('.get-involved-nav').removeClass('pure-button-active');
+		$(".get-involved-content").fadeOut("fast");
+		$(this).addClass('pure-button-active');
+		$("#"+$(this).data('tab')+"-content").fadeIn();
+		event.preventDefault();
+	});
 });
