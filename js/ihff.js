901795f9cfc70864ae7c1bb4933fd26a769a3477
Ben Jackson <benjackson@Bens-MacBook-Pro.local>
2014-03-09 23:26:09 +0000

    programme, credits, gallery

--- js/ihff.js
+++ js/ihff.js
@@ -10,14 +10,48 @@ $(document).ready(function(){
 		$('.about-menu > li').removeClass('pure-menu-selected');
 		$(".about-content").fadeOut("fast");
 		$(this).parent('li').addClass('pure-menu-selected');
-		$("#"+$(this).data('tab')+"-content").fadeIn();
+		var tab = $(this).data('tab');
+		$('.about-content').each(function(){
+			if($(this).data('content') == tab){
+				$(this).fadeIn();
+			}
+		});
 		event.preventDefault();
 	});
 	$('.get-involved-nav').on('click',function(event){
-		$('.get-involved-nav').removeClass('pure-button-active');
+		$('.get-involved-menu > li').removeClass('pure-menu-selected');
 		$(".get-involved-content").fadeOut("fast");
-		$(this).addClass('pure-button-active');
-		$("#"+$(this).data('tab')+"-content").fadeIn();
+		$(this).parent('li').addClass('pure-menu-selected');
+		var tab = $(this).data('tab');
+		$('.get-involved-content').each(function(){
+			if($(this).data('content') == tab){
+				$(this).fadeIn();
+			}
+		});
+		event.preventDefault();
+	});
+	$('.credits-nav').on('click',function(event){
+		$('.credits-menu > li').removeClass('pure-menu-selected');
+		$(".credits-content").fadeOut("fast");
+		$(this).parent('li').addClass('pure-menu-selected');
+		var tab = $(this).data('tab');
+		$('.credits-content').each(function(){
+			if($(this).data('content') == tab){
+				$(this).fadeIn();
+			}
+		});
+		event.preventDefault();
+	});
+	$('.programme-nav').on('click',function(event){
+		$('.programme-menu > li').removeClass('pure-menu-selected');
+		$(".programme-content").fadeOut("fast");
+		$(this).parent('li').addClass('pure-menu-selected');
+		var tab = $(this).data('tab');
+		$('.programme-content').each(function(){
+			if($(this).data('content') == tab){
+				$(this).fadeIn();
+			}
+		});
 		event.preventDefault();
 	});
 });
